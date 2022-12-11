79e192113e9ca3d550c4f143c2bccc2fc5aed35c
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-11 16:46:52 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
